c637b8bd0663f1c5616035fd6aea0c2f782f3298
Matt Corcoran <mkcorcoran2015@gmail.com>
2026-05-05 15:27:58 +0200

    feat: add basic TimeEntry initialisation

--- services/time-tracking/app/models.py
+++ services/time-tracking/app/models.py
@@ -1,2 +1,6 @@
 class TimeEntry:
-    pass
+
+    def __init__(self, owner_user_id, project_id, started_at):
+        self.owner_user_id = owner_user_id
+        self.project_id = project_id
+        self.started_at = started_at
