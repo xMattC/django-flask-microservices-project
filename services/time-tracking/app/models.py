45e22fb2a5fa68d61bc55b290fad2166bd968848
Matt Corcoran <mkcorcoran2015@gmail.com>
2026-05-05 15:39:59 +0200

    feat: support optional fields on TimeEntry

--- services/time-tracking/app/models.py
+++ services/time-tracking/app/models.py
@@ -1,7 +1,8 @@
 class TimeEntry:
 
-    def __init__(self, owner_user_id, project_id, started_at, description=None):
+    def __init__(self, owner_user_id, project_id, started_at, description=None, ended_at=None):
         self.owner_user_id = owner_user_id
         self.project_id = project_id
         self.started_at = started_at
         self.description = description
+        self.ended_at = ended_at
