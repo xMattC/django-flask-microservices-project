3dd4cf6f83ba3c6c331865d91208e68bc8a76002
Matt Corcoran <mkcorcoran2015@gmail.com>
2026-05-05 12:52:02 +0200

    Set-up time-tracking microservice (Flask & Docker)

---
+++ services/time-tracking/tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+
+# from app.extensions import db
+from app.main import create_app
+
+
+@pytest.fixture
+def app():
+    """Create a Flask test app with a clean database.
+
+    return: Configured Flask application instance.
+    """
+    app = create_app()
+    app.config.update({"TESTING": True})
+
+    # with app.app_context():
+    #     db.drop_all()
+    #     db.create_all()
+
+    yield app
+
+    # with app.app_context():
+    #     db.session.remove()
+    #     db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    """Create a Flask test client.
+
+    param app: Flask application instance.
+    return: Flask test client.
+    """
+    return app.test_client()
