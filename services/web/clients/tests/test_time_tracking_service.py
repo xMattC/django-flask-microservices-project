5de73f7b8e0c8653a578a058afb12f9c3e808aa1
Matt Corcoran <mkcorcoran2015@gmail.com>
2026-05-07 12:32:57 +0200

    test: add raises error on invalid response schema test for stop_time_entry

--- services/web/clients/tests/test_time_tracking_service.py
+++ services/web/clients/tests/test_time_tracking_service.py
@@ -498,7 +498,22 @@ class TimeTrackingClientTests(SimpleTestCase):
         with self.assertRaises(TimeTrackingServiceError):
             stop_time_entry(user_id=user_id, time_entry_id=time_entry_id)
 
-    # def test_stop_time_entry_raises_error_on_invalid_response_schema(self):
+    @responses.activate
+    @override_settings(TIME_TRACKING_SERVICE_URL="http://time-tracking:5000")
+    def test_stop_time_entry_raises_error_on_invalid_response_schema(self):
+        """Test stop_time_entry raises TimeTrackingServiceError on invalid response schema."""
+        user_id = 123
+        time_entry_id = 10
+
+        responses.add(
+            method=responses.POST,
+            url=f"http://time-tracking:5000/api/time-entries/{time_entry_id}/stop",
+            json={"unexpected_key": []},
+            status=200,
+        )
+
+        with self.assertRaises(TimeTrackingServiceError):
+            stop_time_entry(user_id=user_id, time_entry_id=time_entry_id)
 
     # -----------------------------------------------------------------------------------------------------------------
     # Test cases for update_time_entry
