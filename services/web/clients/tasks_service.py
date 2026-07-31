bebb856c833211ff5f771a2379e5a72576329d39
Matt Corcoran <mkcorcoran2015@gmail.com>
2026-07-31 22:06:47 +0100

    feat: handles a successful task entry creation client test

--- services/web/clients/tasks_service.py
+++ services/web/clients/tasks_service.py
@@ -10,7 +10,24 @@ class TaskServiceUnavailable(TaskServiceError):
     pass
 
 def create_task(user_id: int, payload: dict):
-    pass
+    """Create a new task entry for a given user.
+
+    Sends a POST request to the Task service and returns the created task entry.
+
+    Parameters:
+    - user_id : The ID of the authenticated user.
+    - payload : The data for the new task entry.
+
+    Returns:
+    - A dictionary representing the created task entry.
+    """
+    url = f"{settings.TASK_SERVICE_URL}/api/tasks"
+
+    response = requests.post(url, json=payload, headers={"X-User-ID": str(user_id)}, timeout=5)
+
+    data = response.json()
+
+    return data["results"][0]
 
 def get_tasks(user_id: int):
     pass
--- services/web/config/settings.py
+++ services/web/config/settings.py
@@ -145,6 +145,7 @@ LOGOUT_REDIRECT_URL = "user:login"
 # Internal Docker Compose service URLs (requires internal container ports)
 PROJECTS_SERVICE_URL = "http://projects:5000"
 TIME_TRACKING_SERVICE_URL = "http://time-tracking:5000"
+TASK_SERVICE_URL = "http://tasks:5000"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
